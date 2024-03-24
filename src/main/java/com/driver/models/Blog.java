587999f626a86474a2984a0daaaedca84c372aaa
Mondal <subrata.b.mondal@capgemini.com>
2024-03-24 12:55:50 +0530

    trying to solve issues

--- src/main/java/com/driver/models/Blog.java
+++ src/main/java/com/driver/models/Blog.java
@@ -11,9 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Entity
 
